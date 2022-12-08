fe0be39460770805b1d428acf5c1742a9fb5c87e
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2022-12-08 02:07:47 -0300

    Fix telegram desktop install script

--- config-files/telegram.desktop_install.sh
+++ config-files/telegram.desktop_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Creates a symbolic link to the latest version of Obsidian, to avoid having
+# Creates a symbolic link to the latest version of the app, to avoid having
 # references to fixed versions.
 ln -s ${HOME}/bin/Telegram/Telegram \
     ${HOME}/bin/Telegram-latest
@@ -10,3 +10,6 @@ ln -s ${HOME}/projects/personal/dev/config-files/telegram.desktop \
 
 ln -s ${HOME}/projects/personal/dev/config-files/telegram.png \
     ${HOME}/.local/share/icons/telegram.png 
+
+# Update desktop files database.
+update-desktop-database ~/.local/share/applications
