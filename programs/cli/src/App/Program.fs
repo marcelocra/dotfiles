f19d8c992718a8057101642e13163b9db9f9a767
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-03-21 23:31:31 -0300

    reformat program

--- programs/cli/src/App/Program.fs
+++ programs/cli/src/App/Program.fs
@@ -5,7 +5,11 @@ open Library
 let main args =
     printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
 
-    let value, json = getJson {| args=args; year=System.DateTime.Now.Year |}
+    let value, json =
+        getJson
+            {| args = args
+               year = DateTime.Now.Year |}
+
     printfn $"Input: %0A{value}"
     printfn $"Output: %s{json}"
 
