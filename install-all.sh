5587e434f93bcabe0007a5bed343b80c851f80d5
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-03-21 21:52:08 -0300

    create simple installation script for all dotfiles

---
+++ install-all.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+main() {
+  local src_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/config-files"
+
+
+  # Vim.
+  ln -s -f "$src_dir/vim/vimrc" "$HOME/.config/nvim/init.vim"
+
+
+  # Git.
+  ln -s -f "$src_dir/.gitconfig" "$HOME/.gitconfig"
+  ln -s -f "$src_dir/.gitconfig.linux.gitconfig" "$HOME/.gitconfig.linux.gitconfig"
+  ln -s -f "$src_dir/.gitconfig.unix.gitconfig" "$HOME/.gitconfig.unix.gitconfig"
+  ln -s -f "$src_dir/.gitconfig.personal.gitconfig" "$HOME/.gitconfig.personal.gitconfig"
+
+
+  # Tmux.
+  ln -s -f "$src_dir/.tmux.conf" "$HOME/.tmux.conf"
+
+
+  # Obsidian.
+  ln -s -f "$src_dir/obsidian.desktop" \
+    ${HOME}/.local/share/applications/obsidian.desktop
+  # Credits to the creator of the icons here: https://forum.obsidian.md/t/big-sur-icon/8121
+  ln -s -f "$src_dir/obsidian.png" \
+    ${HOME}/.local/share/icons/obsidian.png 
+
+
+  # Portacle.
+  ln -s -f "$src_dir/portacle.desktop" \
+    ${HOME}/.local/share/applications/portacle.desktop
+  ln -s -f "$src_dir/portacle.svg" \
+    ${HOME}/.local/share/icons/portacle.svg 
+
+  
+  # # Telegram.
+  # ln -s -f "$src_dir/telegram.desktop" \
+  #   ${HOME}/.local/share/applications/telegram.desktop
+  # ln -s -f "$src_dir/telegram.png" \
+  #   ${HOME}/.local/share/icons/telegram.png 
+
+
+  # # Todoist.
+  # ln -s -f "$src_dir/todoist.desktop" \
+  #   ${HOME}/.local/share/applications/todoist.desktop
+  # ln -s -f "$src_dir/todoist.png" \
+  #   ${HOME}/.local/share/icons/todoist.png 
+
+
+  update-desktop-database ~/.local/share/applications
+  
+
+  # VSCode.
+  ln -s -f "$src_dir/vscode-keybindings.jsonc" \
+    ${HOME}/.config/Code/User/keybindings.json
+  ln -s -f "$src_dir/vscode-settings.jsonc" \
+    ${HOME}/.config/Code/User/settings.json
+  ln -s -f "$src_dir/vscode-snippets.code-snippets" \
+    ${HOME}/.config/Code/User/snippets/snippets.code-snippets
+}
+
+
+# Note: this script REPLACES everything (note the -f option in ln).
+# If you want to keep your existing config files, save them before running this.
+# Uncomment the line below to run the script.
+
+# main
