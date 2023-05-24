9f54ddfb6cff81c91b1bf5ae40724ed320114db7
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2023-05-24 13:25:26 -0300

    Reduce speed of evoluent mouse

--- config-files/init_system_linux.sh
+++ config-files/init_system_linux.sh
@@ -96,6 +96,29 @@ reduce_speed_of_logitech_mouse() {
     xinput set-prop $device_id $prop_id -0.75  # I found this value to be good.
 }
 
+reduce_speed_of_evoluent_mouse() {
+    local device_id
+    device_id=$(xinput \
+        | sed -nre 's/.*Evoluent\ VerticalMouse.*id\=([0-9]+).*/\1/p' \
+        | head -n1)
+    if [ -z "$device_id" ]; then
+        return
+    fi
+
+    local prop_id
+    prop_id=$(xinput list-props $device_id \
+        | sed -nre 's/.*Accel\ Speed\ \(([0-9]+)\).*/\1/p' \
+        | head -n1)
+    if [ -z "$prop_id" ]; then
+        return
+    fi
+
+    xinput set-prop $device_id $prop_id -0.75  # I found this value to be good.
+
+}
+
 setup_expert_mouse
 setup_evoluent_mouse
 reduce_speed_of_logitech_mouse
+reduce_speed_of_evoluent_mouse
+
