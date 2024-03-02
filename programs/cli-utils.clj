e21bc391be59605e535c5654e1fa1de765753c42
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-03-02 17:52:57 -0300

    create prettierrc generator

--- programs/cli-utils.clj
+++ programs/cli-utils.clj
@@ -4,7 +4,43 @@
   '[clojure.java.shell :refer [sh]]
   '[clojure.string :as str]
   '[babashka.fs :as fs]
-  '[babashka.cli :as cli])
+  '[babashka.cli :as cli]
+  '[clojure.java.io :as io])
+
+(def
+  prettierrc-content
+  "{
+  \"arrowParens\": \"always\",
+  \"bracketSpacing\": true,
+  \"endOfLine\": \"lf\",
+  \"htmlWhitespaceSensitivity\": \"css\",
+  \"insertPragma\": false,
+  \"singleAttributePerLine\": false,
+  \"bracketSameLine\": false,
+  \"jsxBracketSameLine\": false,
+  \"jsxSingleQuote\": false,
+  \"printWidth\": 120,
+  \"proseWrap\": \"preserve\",
+  \"quoteProps\": \"as-needed\",
+  \"requirePragma\": false,
+  \"semi\": true,
+  \"singleQuote\": false,
+  \"tabWidth\": 2,
+  \"trailingComma\": \"es5\",
+  \"useTabs\": false,
+  \"embeddedLanguageFormatting\": \"auto\",
+  \"vueIndentScriptAndStyle\": false,
+  \"overrides\": [
+    {
+      \"files\": \"*.md\",
+      \"options\": {
+        \"printWidth\": 80,
+        \"proseWrap\": \"always\",
+        \"parser\": \"markdown\"
+      }
+    }
+  ]
+}")
 
 (defn free-ram
   "Prints the amount of free ram memory."
@@ -14,6 +50,16 @@
       (:out (sh "cat" "/proc/meminfo"))
       (re-find #"MemFree.*")
       (re-find #"[0-9]+.*"))))
+
+(defn js-pretty
+  "Creates a base JavaScript prettierrc file with markdown override."
+  []
+  (spit
+    (-> (fs/cwd) 
+        (.toString)
+        (io/file ".prettierrc")
+        (.toString))
+    prettierrc-content))
 
 
 ;; CLI helper ------------------------------------------------------------------
@@ -27,8 +73,9 @@
   (println
     (str "Choose one of the commands below: \n\n"
          (str/join "\n"
-                   (map #(format "%s\t%s" (:name %) (:doc %))
-                        [(meta #'free-ram)])))))
+                   (map #(format "%-25s%s" (:name %) (:doc %))
+                        [(meta #'free-ram)
+                         (meta #'js-pretty)])))))
 
 (def parsed-cli-args
   (cli/parse-opts *command-line-args* {:spec cli-options}))
