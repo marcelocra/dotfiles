17ba5027754372e2d39e532b45ec772ad756f1a7
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-03-08 15:46:32 -0300

    feat(vscode): improve toggle for light/dark modes

--- programs/toggle-vscode-light-dark.clj
+++ programs/toggle-vscode-light-dark.clj
@@ -1,19 +1,29 @@
 #!/usr/bin/env bb
 
 (require 
-  '[clojure.string :as str]
+  '[clojure.string :as s]
   '[clojure.java.io :as io]
   '[babashka.fs :as fs]
-  '[clojure.pprint :as pp]
-  '[clojure.reflect :as reflect])
-
-(def settings-content (slurp settings-path))
+  '[clojure.pprint :as pp :refer [pprint]]
+  '[clojure.reflect :as reflect]
+  '[clojure.edn :as edn])
 
 ;; This works on Linux, but might not work on other systems.
 (def settings-path (-> (System/getenv "HOME")
                        (io/file ".config" "Code" "User" "settings.json")
                        (fs/canonicalize)  ;; Necessary to follow symlinks (`slurp` doesn't).
                        (.toString)))
+
+(def settings-content (slurp settings-path))
+
+;; Check some settings lines.
+(comment
+  (map
+    #(pprint (edn/read-string (get % 1)))
+    (re-seq #".*(\{:dark.*)" settings-content))
+
+  ;; rcf - rich comment form
+  )
 
 ;; TODO: add more options and allow choosing from cmd line.
 (def theme-mapper
@@ -26,13 +36,36 @@
                     theme)]
     [(:light current) (:dark current)]))
 
+(defn update-colors [settings-content]
+  (let [matches (re-seq #"(.*)//(\{:dark.*)" settings-content)] 
+    (loop [not-processed matches
+           current-content settings-content
+           abort-counter 1]
+      (if (or 
+            (> abort-counter 1000) 
+            (<= (count not-processed) 0))
+        current-content
+        (let [currently-processing (first not-processed)
+              opts (edn/read-string (get currently-processing 2))
+              line-content-to-replace (get currently-processing 1)
+              is-light? (not (nil? (re-find (re-pattern (:light opts)) line-content-to-replace)))
+              replace-with (s/replace line-content-to-replace 
+                                      (re-pattern (if is-light? (:light opts) (:dark opts))) 
+                                      (if is-light? (:dark opts) (:light opts)))
+              replaced-content (s/replace current-content
+                                          line-content-to-replace
+                                          replace-with)] 
+          (recur (next not-processed) 
+                 replaced-content
+                 (inc abort-counter)))))))
+
 (defn from-theme-to-theme [from to]
   (do
     (println 
       (format "from '%s' to '%s'" from to))
     (spit settings-path
-          (str/replace
-            settings-content 
+          (s/replace
+            (update-colors settings-content) 
             (re-pattern from)
             to))))
 
@@ -49,8 +82,11 @@
 (toggle)
 
 
-;; playground below! :)
+;; ---------------------------------------------------------------------------------------------------------------------
+;; ----------- PLAYGROUND ARCHIVES :) ----------------------------------------------------------------------------------
+;; ---------------------------------------------------------------------------------------------------------------------
 
+;; changing the color scheme from light/dark/light
 (comment
   (re-find #"colorTheme.*Dark" settings-content)
 
@@ -60,7 +96,7 @@
     (sort-by :name)
     (pp/print-table))
 
-  (pp/pprint (set (map #(.getName %) (vec (.getMethods (.getClass settings-path))))))
+  (pprint (set (map #(.getName %) (vec (.getMethods (.getClass settings-path))))))
   (-> settings-path
       (.getClass)
       (.getFields))
@@ -71,5 +107,49 @@
   (pprint (map #(.getName %) (-> settings-path class .getMethods)))
   
   ;; rcf
+  )
+
+;; changing settings colors based on an edn comment
+(comment
+  ;; read file if necessary
+  #_(def settings-content (slurp settings-path))
+
+  ;; to allow playing without actually nuking the file content
+  (defonce backup-settings-content settings-content)
+  (defn reset-settings []
+    (s/split settings-path backup-settings-content))
+
+  ;; if this line matches, potentially that there are colors to be changed
+  (def matches (re-seq #"(.*)//(\{:dark.*)" settings-content))
+  (map #(pprint (edn/read-string (get % 2))) 
+       (re-seq #"(.*)//(\{:dark.*)" settings-content))
+  (count matches)
+  (first matches)
+  (rest matches)
+  (type matches)
+  (count (next matches))
+  (type (next matches))
+  (type (rest matches))
+
+  (defn p [stuff]
+    (pprint stuff))
+
+  #_(spit settings-path backup-settings-content)
+  #_(spit settings-path (get @new-contents (- (count @new-contents) 1)))
+  (spit settings-path (new-file-content (slurp settings-path)))
+
+
+  ;; checking file contents
+
+  (def atom-content {:id 1})
+  (defonce new-contents (atom atom-content))
+  (reset! new-contents atom-content)
+  (do 
+    (swap! new-contents update :id inc)
+    (swap! new-contents assoc (:id @new-contents) (new-file-content)))
+  @new-contents
+  (count @new-contents)
+
+  ;; rcf - rich comment form
   )
 
