140b3a30789bec34470863c6814d98d6b1646bbc
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-03-20 02:26:11 -0300

    update mng to sort results by modified time

--- programs/projects_manager.clj
+++ programs/projects_manager.clj
@@ -1,67 +1,73 @@
 #!/usr/bin/env bb
 
+;; idea: symlink this as mng (for manager).
+
 (ns projects-manager
   (:require
     [babashka.fs :as fs]
     [clojure.string :as s]
-    [clojure.pprint :refer [pprint] :rename {pprint p}]
+    [clojure.pprint :as pp :refer [pprint] :rename {pprint p}]
     [clojure.java.shell :refer [sh]]
     [clojure.java.io :as io]))
 
-(defonce projects-dir (System/getenv "MCRA_PROJECTS_FOLDER"))
-(defonce files (fs/list-dir projects-dir))
+(def projects-dir (System/getenv "MCRA_PROJECTS_FOLDER"))
+(def folders (->> (fs/list-dir projects-dir)
+                  (filter #(fs/directory? %))))
 
-(def folder-categories {:backed-up #{}
-                        :to-review #{} 
-                        :no-git #{}})
+(defn sorted-set-by-modified-time-desc []
+  (sorted-set-by 
+    (fn [a b] 
+      (.compareTo (fs/last-modified-time b) 
+                  (fs/last-modified-time a)))))
+
+(def folder-categories {:backed-up (sorted-set-by-modified-time-desc)
+                        :to-review (sorted-set-by-modified-time-desc)
+                        :no-git (sorted-set-by-modified-time-desc)})
 
 (def init-db (merge folder-categories 
-                    {:total-files (count files) 
-                     :total-files-touched 0}))
+                    {:number-of-folders (count folders)}))
 
 (def db (atom init-db))
 
 (do
-  (swap! db assoc :total-files-touched 0)
-  (doseq [file files]
-    (let [visited? (or 
-                     (contains? (:backed-up @db) file) 
-                     (contains? (:to-review @db) file))] 
+  (doseq [folder folders]
+    (let [visited? (some #(contains? (% @db) folder) (keys folder-categories))] 
       (if visited?
         nil  ;; File already processed, nothing to do.
-        (if (not (fs/directory? file))
-          (swap! db update :total-files-touched inc)  ;; Bail, we only care for directories.
-          (let [{:keys [out err]} (sh "git" "status" :dir (.toString file))
-                to-update (cond
-                            (not (empty? err))
-                            (if (re-find #"fatal: not a git repository" err)
-                              :no-git
-                              :unknown-error)
+        (let [{:keys [out err]} (sh "git" "status" :dir (.toString folder))
+              to-update 
+              (cond
+                (not (empty? err))
+                (if (re-find #"fatal: not a git repository" err)
+                  :no-git
+                  :unknown-error)
 
-                            (and (re-find #"Your branch is up to date with" out)
-                                 (re-find #"nothing to commit, working tree clean" out))
-                            :backed-up
+                (and (re-find #"Your branch is up to date with" out)
+                     (re-find #"nothing to commit, working tree clean" out))
+                :backed-up
 
-                            :else
-                            :to-review)]
-            (swap! db update to-update conj file))))))
-  (swap! db update :total-files-touched 
-         (fn [initial-value] 
-           (reduce + initial-value 
-                   (map 
-                     #(count (% @db)) 
-                     (keys folder-categories)))))
+                :else
+                :to-review)]
+          (swap! db update to-update conj folder)))))
   (p @db))
-
 
 
 ;; learning the api
 
 (comment
   (def dir (fs/list-dir "."))
+  (def first-el (first dir))
+  (def last-el (last dir))
 
-  (first dir)
-  (last dir)
+  (.compareTo (fs/last-modified-time first-el) (fs/last-modified-time last-el))
+  (.toString (fs/last-modified-time first-el))
+
+  (-> (fs/last-modified-time first-el)
+      (.getClass)
+      (.getMethods)
+      (->> (map #(.getName %)))
+      (p))
+
 
   (fs/list-dir dir)
   (sh "git" "status" "--porcelain" :dir (.toString dir))
