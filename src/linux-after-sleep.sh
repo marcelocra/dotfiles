6798f52f11872a3f042f7a6d487e0e2c8e9ef593
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-12-20 09:57:16 -0300

    feat: create script to deal with linux suspend

--- src/linux-after-sleep.sh
+++ src/linux-after-sleep.sh
@@ -1,8 +1,76 @@
 #!/usr/bin/env bash
+
 #
-# https://stackoverflow.com/a/58411207/1814970
+# From `man pm-action`:
+#
+#   /etc/pm/sleep.d, /usr/lib/pm-utils/sleep.d: Programs in these directories
+#   (called hooks) are combined and executed in C sort order before suspend and
+#   hibernate with as argument suspend or hibernate. Afterwards, they are called
+#   in reverse order with argument resume and thaw respectively. If both
+#   directories contain a similar named file, the one in /etc/pm/sleep.d will
+#   get preference. It is possible to disable a hook in the distribution
+#   directory by putting a non-executable file in /etc/pm/sleep.d, or by adding
+#   it to the HOOK_BLACKLIST configuration variable
+#
+# Reference: https://stackoverflow.com/a/58411207/1814970
+#
+
+set -e
 
 action="$1"
+this_file_name=$(basename $0)
+log_file="$HOME/.${USER}.${this_file_name}.log"
+
+log() {
+    echo "[`date`] $@" | tee -a $log_file
+}
+
+log "---------------------------------------------------------------------"
+
+if [[ "$action" == "install-this-script" ]]; then
+    install_path="/etc/pm/sleep.d/99_linux_after_sleep"
+
+    if [[ -f $install_path ]]; then
+        log "The file '$install_path' already exists. Exiting..."
+        exit 1
+    fi
+
+    this_script_path=$(realpath $0)
+
+    log ""
+    log "Will symlink this script:"
+    log ""
+    log "  $this_script_path"
+    log ""
+    log "to:"
+    log ""
+    log "  $install_path"
+    log ""
+    log "To uninstall it, just remove the file. Continue? [y/n]"
+    echo -n "> "
+
+    read -r answer
+    log "Your answer: $answer"
+    if [[ "$answer" != "y" ]]; then
+        log "Exiting..."
+        exit 1
+    fi
+
+    all_installed_scripts_log="$HOME/.$USER.all-installed-scripts.log"
+
+    sudo ln -s $this_script_path /etc/pm/sleep.d/99_linux_after_sleep \
+        && (log "Done!" && log "Installed '$this_script_path' to '$install_path'" >> $all_installed_scripts_log) \
+        || (log "Failed to copy the file. Exiting..." && exit 1)
+
+    exit 0
+fi
+
+# We only want to blow up when not running in the context of pm-action.
+set +e
+
+echo "---------------------------------------------------------------------" >> $log_file
+log $action
+echo "---------------------------------------------------------------------" >> $log_file
 
 case "$action" in
    suspend)
@@ -12,6 +80,11 @@ case "$action" in
    resume)
         # List of programs to when the systems "resumes"
         # after being suspended
+
+        log "Resuming the system..."
+        $HOME/projects/dotfiles/linux-startup-config.sh >> $log_file 2>&1 \
+            && log "Done!" \
+            || log "Failed to run the startup script. Exiting..."
    ;;
    hibernate)
         # List of programs to run before the system hibernates
