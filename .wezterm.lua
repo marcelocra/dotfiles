88b2271b97e1ea046a57ec7a5746b611fe69832b
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-03-01 02:44:58 -0300

    refactor(wez): improved readability and organization

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,264 +13,299 @@ local mux = wezterm.mux
 -- Options: 'Fixed', 'Fixed Extended', 'Extended'.
 local which_iosevka = "Iosevka Fixed Extended"
 
+local good_font_size = 11
 local fonts = {
-  meslo = wezterm.font({
-    -- family = 'MesloLGL Nerd Font', -- more space between lines
-    -- family = 'MesloLGM Nerd Font', -- medium space between lines
-    family = "MesloLGS Nerd Font", -- less space between lines
-    harfbuzz_features = {
-      "cv10 10",
-    },
-    weight = 400,
-  }),
+  meslo = {
+    font = wezterm.font({
+      -- family = 'MesloLGL Nerd Font', -- more space between lines
+      family = "MesloLGM Nerd Font", -- medium space between lines
+      -- family = "MesloLGS Nerd Font", -- less space between lines
+      harfbuzz_features = {
+        -- "cv10 10",
+      },
+      weight = 400,
+    }),
+    size = good_font_size,
+  },
 
-  jb = wezterm.font({
-    family = "JetBrains Mono",
-    weight = 400,
-    -- Ligature test:
-    --  ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~
-    --  ~ < > = + _ _ ( ) * & ^ % $ # @ ! { } [ ] \ | / ? : ; , . ` ' " ´ ` ˜
-    --  => -> <> <= >= != !== === ~= |> <- --> || .- :- .= {..} 1/2 3/8 100/48
-    --  a b c d e f g h i j k l m n o p q r s t u v x w y z
-    --  A B C D E F G H I J K L M N O P Q R S T U V X W Y Z
-    harfbuzz_features = {
-      -- Minimal: default style (ss01); no ligatures in `=` (ss19); raised bar `f` (ss20); dashed
-      -- zero (zero); highlight some weird glyphs (cv99).
-      -- 'ss01', 'ss19', 'ss20', 'zero', 'cv99',
+  jb = {
+    font = wezterm.font({ -- <<(
+      family = "JetBrains Mono",
+      weight = 400,
+      -- Ligature test:
+      --  ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~
+      --  ~ < > = + _ _ ( ) * & ^ % $ # @ ! { } [ ] \ | / ? : ; , . ` ' " ´ ` ˜
+      --  => -> <> <= >= != !== === ~= |> <- --> || .- :- .= {..} 1/2 3/8 100/48
+      --  a b c d e f g h i j k l m n o p q r s t u v x w y z
+      --  A B C D E F G H I J K L M N O P Q R S T U V X W Y Z
+      harfbuzz_features = {
+        -- Minimal: default style (ss01); no ligatures in `=` (ss19); raised bar `f` (ss20); dashed
+        -- zero (zero); highlight some weird glyphs (cv99).
+        -- 'ss01', 'ss19', 'ss20', 'zero', 'cv99',
 
-      -- Different: closed construction (ss02); no ligatures in `=` (ss19); raised bar `f` (ss20);
-      -- dashed zero (zero); different `g`, `j`, `Q`, `8`, `5` (respectively); highlight some weird
-      -- glyphs (cv99).
-      -- 'ss02', 'ss19', 'ss20', 'zero', 'cv03', 'cv04', 'cv16', 'cv19', 'cv20', 'cv99',
-    },
-  }),
+        -- Different: closed construction (ss02); no ligatures in `=` (ss19); raised bar `f` (ss20);
+        -- dashed zero (zero); different `g`, `j`, `Q`, `8`, `5` (respectively); highlight some weird
+        -- glyphs (cv99).
+        -- 'ss02', 'ss19', 'ss20', 'zero', 'cv03', 'cv04', 'cv16', 'cv19', 'cv20', 'cv99',
+      },
+    }), -- )>>
+    size = 10,
+  },
 
-  jb_nf = wezterm.font({
-    family = "JetBrainsMono Nerd Font",
-    weight = 400,
-    -- Ligature test:
-    --  ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~
-    --  ~ < > = + _ _ ( ) * & ^ % $ # @ ! { } [ ] \ | / ? : ; , . ` ' " ´ ` ˜
-    --  => -> <> <= >= != !== === ~= |> <- --> || .- :- .= {..} 1/2 3/8 100/48
-    --  a b c d e f g h i j k l m n o p q r s t u v x w y z
-    --  A B C D E F G H I J K L M N O P Q R S T U V X W Y Z
-    harfbuzz_features = {
-      -- 'zero'
-      -- 'ss01', 'ss19', 'ss20', 'zero', 'cv99',
-      "ss02",
-      "ss19",
-      "ss20",
-      "zero",
-      "cv03",
-      "cv04",
-      "cv16",
-      "cv18",
-      "cv19",
-      "cv20",
-      "cv99",
-    },
-  }),
+  jb_nf = {
+    font = wezterm.font({ -- <<(
+      family = "JetBrainsMono Nerd Font",
+      weight = 400,
+      -- Ligature test:
+      --  ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~
+      --  ~ < > = + _ _ ( ) * & ^ % $ # @ ! { } [ ] \ | / ? : ; , . ` ' " ´ ` ˜
+      --  => -> <> <= >= != !== === ~= |> <- --> || .- :- .= {..} 1/2 3/8 100/48
+      --  a b c d e f g h i j k l m n o p q r s t u v x w y z
+      --  A B C D E F G H I J K L M N O P Q R S T U V X W Y Z
+      harfbuzz_features = {
+        -- 'zero'
+        -- 'ss01', 'ss19', 'ss20', 'zero', 'cv99',
+        "ss02",
+        "ss19",
+        "ss20",
+        "zero",
+        "cv03",
+        "cv04",
+        "cv16",
+        "cv18",
+        "cv19",
+        "cv20",
+        "cv99",
+      },
+    }), -- )>>
+    size = 10,
+  },
 
-  cascadia = wezterm.font({
-    -- family = 'Cascadia Code NF',
-    family = "CaskaydiaCove NF",
-    weight = 400,
-    harfbuzz_features = {
-      "calt 0",
-      "ss01",
-      "ss19",
-      "ss20",
-    },
-  }),
+  cascadia = {
+    font = wezterm.font({
+      -- family = 'Cascadia Code NF',
+      family = "CaskaydiaCove NF",
+      weight = 400,
+      harfbuzz_features = {
+        "calt 0",
+        "ss01",
+        "ss19",
+        "ss20",
+      },
+    }),
+    size = good_font_size,
+  },
 
-  fira = wezterm.font({
-    family = "Fira Code",
-    harfbuzz_features = {
-      "calt 0",
-      "cv01",
-      "cv02",
-      "cv05",
-      "cv09",
-      "ss05",
-      "ss03",
-      "cv16",
-      "cv30",
-      "cv25",
-      "cv26",
-      "cv32",
-      "cv28",
-      "ss10",
-      "cv14",
-      "cv12",
-    },
-  }),
+  fira = {
+    font = wezterm.font({
+      family = "Fira Code",
+      harfbuzz_features = {
+        "calt 0",
+        "cv01",
+        "cv02",
+        "cv05",
+        "cv09",
+        "ss05",
+        "ss03",
+        "cv16",
+        "cv30",
+        "cv25",
+        "cv26",
+        "cv32",
+        "cv28",
+        "ss10",
+        "cv14",
+        "cv12",
+      },
+    }),
+    size = good_font_size,
+  },
 
-  geist = wezterm.font({
+  geist = { font = wezterm.font({
     family = "Geist Mono",
-  }),
+  }), size = good_font_size },
 
-  noto = wezterm.font({
-    family = "Noto Sans Mono",
-    weight = 400,
-  }),
+  noto = {
+    font = wezterm.font({
+      family = "Noto Sans Mono",
+      weight = 400,
+    }),
+    size = 11,
+    line_height = 0.9,
+  },
 
-  dm = wezterm.font({
-    family = "DM Mono",
-    weight = 700,
-    harfbuzz_features = {
-      "ss01", -- rounded commas
-      "ss03", -- non curvy 'g'
-      "ss04", -- non curvy '6' and '9'
-    },
-  }),
+  dm = {
+    font = wezterm.font({
+      family = "DM Mono",
+      weight = 700,
+      harfbuzz_features = {
+        "ss01", -- rounded commas
+        "ss03", -- non curvy 'g'
+        "ss04", -- non curvy '6' and '9'
+      },
+    }),
+    size = good_font_size,
+  },
 
-  scp = wezterm.font({
-    family = "SourceCodeVF",
-    -- weight = 700,
-    harfbuzz_features = {
-      "cv17",
-      "cv01",
-      "cv02",
-      "zero",
-    },
-  }),
+  scp = {
+    font = wezterm.font({
+      family = "SourceCodeVF",
+      -- weight = 700,
+      harfbuzz_features = {
+        "cv17",
+        "cv01",
+        "cv02",
+        "zero",
+      },
+    }),
+    size = good_font_size,
+  },
 
-  iosevka = wezterm.font({
-    family = which_iosevka,
-    weight = 400,
-    harfbuzz_features = {
-      -- Font alternatives. <<(
-      -- ss01: Andale Mono
-      -- ss02: Anonymous Pro
-      -- ss03: Consolas
-      -- ss04: Menlo
-      -- ss05: Fira Mono
-      -- ss06: Liberation Mono
-      -- ss07: Monaco
-      -- ss08: Pragmata Pro
-      -- ss09: Source Code Pro
-      -- ss10: Envy Code R
-      -- ss11: X Window
-      -- ss12: Ubuntu Mono
-      -- ss13: Lucida
-      -- ss14: JetBrains Mono
-      -- ss15: IBM Plex Mono
-      -- ss16: PT Mono
-      -- ss17: Recursive Mono
-      -- ss18: Input Mono
-      -- ss20: Curly
-      -- )>>
+  iosevka = {
+    font = wezterm.font({
+      family = which_iosevka,
+      weight = 400,
+      harfbuzz_features = {
+        -- Font alternatives. <<(
+        -- ss01: Andale Mono
+        -- ss02: Anonymous Pro
+        -- ss03: Consolas
+        -- ss04: Menlo
+        -- ss05: Fira Mono
+        -- ss06: Liberation Mono
+        -- ss07: Monaco
+        -- ss08: Pragmata Pro
+        -- ss09: Source Code Pro
+        -- ss10: Envy Code R
+        -- ss11: X Window
+        -- ss12: Ubuntu Mono
+        -- ss13: Lucida
+        -- ss14: JetBrains Mono
+        -- ss15: IBM Plex Mono
+        -- ss16: PT Mono
+        -- ss17: Recursive Mono
+        -- ss18: Input Mono
+        -- ss20: Curly
+        -- )>>
 
-      -- Others:
-      -- Lucida Console with some changes.
-      -- 'ss13', 'cv02 4', 'cv04 11', 'cv05 8', 'cv10 10',
-      -- 'ss13', 'cv02 1', 'cv04 3', 'cv05 4', 'cv10 25',
-    },
-  }),
+        -- Others:
+        -- Lucida Console with some changes.
+        -- 'ss13', 'cv02 4', 'cv04 11', 'cv05 8', 'cv10 10',
+        -- 'ss13', 'cv02 1', 'cv04 3', 'cv05 4', 'cv10 25',
+      },
+    }),
+    size = good_font_size,
+  },
 
-  iosevka_jb = wezterm.font({
-    family = which_iosevka,
-    weight = 400,
-    harfbuzz_features = {
-      "ss14",
-    },
-  }),
+  iosevka_jb = {
+    font = wezterm.font({
+      family = which_iosevka,
+      weight = 400,
+      harfbuzz_features = {
+        "ss14",
+      },
+    }),
+    size = good_font_size,
+  },
 
-  iosevka_lucida = wezterm.font({
-    family = which_iosevka,
-    weight = 400,
-    harfbuzz_features = {
-      "ss13",
-      "cv10 7",
-      -- Variations.
-      -- 'ss13', 'cv02 4', 'cv04 11', 'cv05 8', 'cv10 10',
-      -- 'ss13', 'cv02 1', 'cv04 3', 'cv05 4', 'cv10 25',
-    },
-  }),
+  iosevka_lucida = {
+    font = wezterm.font({
+      family = which_iosevka,
+      weight = 400,
+      harfbuzz_features = {
+        "ss13",
+        "cv10 7",
+        -- Variations.
+        -- 'ss13', 'cv02 4', 'cv04 11', 'cv05 8', 'cv10 10',
+        -- 'ss13', 'cv02 1', 'cv04 3', 'cv05 4', 'cv10 25',
+      },
+    }),
+    size = good_font_size,
+  },
 
-  iosevka_menlo = wezterm.font({
-    family = which_iosevka,
-    weight = 400,
-    harfbuzz_features = {
-      "ss04",
-    },
-  }),
+  iosevka_menlo = {
+    font = wezterm.font({
+      family = which_iosevka,
+      weight = 400,
+      harfbuzz_features = {
+        "ss04",
+      },
+    }),
+    size = 15,
+  },
 
-  iosevka_monaco = wezterm.font({
-    family = which_iosevka,
-    weight = 400,
-    harfbuzz_features = {
-      "ss07",
-    },
-  }),
+  iosevka_monaco = {
+    font = wezterm.font({
+      family = which_iosevka,
+      weight = 400,
+      harfbuzz_features = {
+        "ss07",
+      },
+    }),
+    size = 15,
+  },
 
-  victor_mono = wezterm.font({
-    family = "Victor Mono",
-    weight = 600,
-    harfbuzz_features = {
-      -- 'calt=0', -- no ligatures (particularly for `==` and `!=`)
-      -- 'ss01', -- different 'a'
-      "ss02", -- different dashed zero (up to ss05)
-      "ss06", -- crossed '7'
-      "ss07", -- different '6' and  '9'
-      "ss08", -- more fish-like stuff (::<)
-      -- |> >- <-> <| <> |- .- :: -.- -> => == === != !== ::<
-    },
-  }),
+  victor_mono = {
+    font = wezterm.font({
+      family = "Victor Mono",
+      weight = 600,
+      harfbuzz_features = {
+        -- 'calt=0', -- no ligatures (particularly for `==` and `!=`)
+        -- 'ss01', -- different 'a'
+        "ss02", -- different dashed zero (up to ss05)
+        "ss06", -- crossed '7'
+        "ss07", -- different '6' and  '9'
+        "ss08", -- more fish-like stuff (::<)
+        -- |> >- <-> <| <> |- .- :: -.- -> => == === != !== ::<
+      },
+    }),
+    size = good_font_size,
+  },
 
-  code_new_roman = wezterm.font({
-    family = "CodeNewRoman Nerd Font",
-    weight = 400,
-  }),
+  code_new_roman = {
+    font = wezterm.font({
+      family = "CodeNewRoman Nerd Font",
+      weight = 400,
+    }),
 
-  recursive = wezterm.font({
-    family = "RecMonoLinear Nerd Font",
-    weight = 400,
-  }),
+    size = good_font_size,
+  },
 
-  recursive_alt = wezterm.font({
-    family = "RecMonoDuotone Nerd Font",
-    weight = 400,
-  }),
+  recursive = {
+    font = wezterm.font({
+      family = "RecMonoLinear Nerd Font",
+      weight = 400,
+    }),
+    size = good_font_size,
+  },
 
-  hack = wezterm.font({
+  recursive_alt = {
+    font = wezterm.font({
+      family = "RecMonoDuotone Nerd Font",
+      weight = 400,
+    }),
+    size = good_font_size,
+  },
+
+  hack = { font = wezterm.font({
     family = "Hack Nerd Font",
     weight = 400,
-  }),
+  }), size = good_font_size },
 
-  blex = wezterm.font({
+  blex = { font = wezterm.font({
     family = "Blex Mono Nerd Font",
     weight = 400,
-  }),
+  }), size = good_font_size },
 
-  commit = wezterm.font({
-    family = "CommitMono Nerd Font",
-    weight = 400,
-  }),
+  commit = {
+    font = wezterm.font({
+      family = "CommitMono Nerd Font",
+      weight = 400,
+    }),
+    size = good_font_size,
+  },
 }
 
-local common_good_size = 13
-local fonts_size = {
-  cascadia = common_good_size,
-  fira = common_good_size,
-  geist = common_good_size,
-  noto = 20,
-  dm = common_good_size,
-  meslo = common_good_size,
-  iosevka = common_good_size,
-  iosevka_jb = common_good_size,
-  iosevka_lucida = common_good_size,
-  iosevka_menlo = 15,
-  iosevka_monaco = 15,
-  jb = 11,
-  victor_mono = common_good_size,
-  code_new_roman = common_good_size,
-  recursive = common_good_size,
-  recursive_alt = common_good_size,
-  hack = common_good_size,
-  blex = common_good_size,
-  commit = common_good_size,
-}
 -- )>>
 
 local config = wezterm.config_builder()
@@ -547,47 +582,17 @@ config.window_background_opacity = 1
 config.text_background_opacity = 1
 config.window_decorations = "RESIZE"
 
--- Only the last one will be applied. fff
-config.font = fonts.code_new_roman
-config.font_size = fonts_size.code_new_roman
-config.font = fonts.iosevka
-config.font_size = fonts_size.iosevka
-config.font = fonts.iosevka_jb
-config.font_size = fonts_size.iosevka_jb
-config.font = fonts.iosevka_lucida
-config.font_size = fonts_size.iosevka_lucida
-config.font = fonts.dm
-config.font_size = fonts_size.dm
-config.font_size = fonts_size.jb
-config.font = fonts.noto
-config.font_size = fonts_size.noto
-config.font = fonts.recursive_duotone
-config.font_size = fonts_size.recursive_duotone
-config.font = fonts.recursive_alt
-config.font_size = fonts_size.recursive_alt
-config.font = fonts.blex
-config.font_size = fonts_size.blex
-config.font = fonts.commit
-config.font_size = fonts_size.commit
-config.font = fonts.hack
-config.font_size = fonts_size.hack
-config.font = fonts.recursive
-config.font_size = fonts_size.recursive
-config.font = fonts.iosevka_monaco
-config.font_size = fonts_size.iosevka_monaco
-config.font = fonts.iosevka_menlo
-config.font_size = fonts_size.iosevka_menlo
-config.font = fonts.victor_mono
-config.font_size = fonts_size.victor_mono
-config.font = fonts.cascadia
-config.font_size = fonts_size.cascadia
-config.font = fonts.meslo
-config.font_size = fonts_size.meslo
-config.font = fonts.jb_nf
-config.font_size = fonts_size.jb
-config.font = fonts.jb
+local set_font = function(font)
+  config.font = font.font
+  config.font_size = font.size
+  config.line_height = font.line_height or 1.0
+end
 
--- config.line_height = 1.2
+set_font(fonts.jb_nf)
+set_font(fonts.noto)
+set_font(fonts.jb)
+set_font(fonts.meslo)
+
 config.freetype_load_target = "Light"
 config.freetype_render_target = "HorizontalLcd"
 -- config.freetype_load_flags = 'NO_HINTING'
