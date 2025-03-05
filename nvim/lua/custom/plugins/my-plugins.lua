8846ed612498e9868e91a1f8226fc31a1287b48c
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-03-05 03:11:16 -0300

    feat(nvim): enhance Copilot integration with custom key mappings and color scheme support

--- nvim/lua/custom/plugins/my-plugins.lua
+++ nvim/lua/custom/plugins/my-plugins.lua
@@ -32,17 +32,53 @@ return {
   },
 
   -- Copilot support.
-  { 'github/copilot.vim' },
+  {
+    'github/copilot.vim',
+    init = function()
+      vim.g.copilot_no_tab_map = true
+    end,
+    config = function()
+      vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+
+      vim.keymap.set('i', '<C-l>', '<Plug>(copilot-accept-word)')
+
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        pattern = 'solarized',
+        -- group = ...,
+        callback = function()
+          vim.api.nvim_set_hl(0, 'CopilotSuggestion', {
+            fg = '#555555',
+            ctermfg = 8,
+            force = true,
+          })
+        end,
+      })
+
+      -- Default mappings:
+
+      -- <C-]>       Dismiss the current suggestion.                               <Plug>(copilot-dismiss)
+      -- <M-]>       Cycle to the next suggestion, if one is available.            <Plug>(copilot-next)
+      -- <M-[>       Cycle to the previous suggestion.                             <Plug>(copilot-previous)
+      -- <M-\>       Explicitly request a suggestion, even if Copilot is disabled. <Plug>(copilot-suggest)
+      -- <M-Right>   Accept the next word of the current suggestion.               <Plug>(copilot-accept-word)
+      -- <M-C-Right> Accept the next line of the current suggestion.               <Plug>(copilot-accept-line)
+
+      --
+    end,
+  },
 
   {
     -- Align stuff.
     'junegunn/vim-easy-align',
     config = function()
       -- Start interactive EasyAlign in visual mode (e.g. vipga)
-      vim.cmd 'xnoremap ga <Plug>(EasyAlign)'
+      vim.keymap.set('x', 'ga', '<Plug>(EasyAlign)')
 
       -- Start interactive EasyAlign for a motion/text object (e.g. gaip)
-      vim.cmd 'nnoremap ga <Plug>(EasyAlign)'
+      vim.keymap.set('n', 'ga', '<Plug>(EasyAlign)')
     end,
   },
 
@@ -60,12 +96,12 @@ return {
 
   --
   --
-  -- CLOJURE, LISPS, PYTHON, LUA
+  -- CLOJURE, LISPS, PYTHON, ETC
   --
 
   {
     'Olical/conjure',
-    ft = { 'clojure', 'fennel', 'python', 'racket', 'lua' }, -- etc
+    ft = { 'clojure', 'fennel', 'python', 'racket' }, -- etc
     lazy = true,
     init = function()
       -- Set configuration options here
