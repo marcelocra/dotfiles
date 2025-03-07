183a0f1b78b7eb30f1d3568ff4128fc4bdcd6813
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-03-07 18:49:50 -0300

    fix(nvim): adjust daylight end time and improve colorscheme mode handling

--- neovim/nvim/lua/custom/time-based-colorscheme.lua
+++ neovim/nvim/lua/custom/time-based-colorscheme.lua
@@ -6,17 +6,21 @@
 --]]
 
 local hour_now = tonumber(os.date '%H')
-local daylight_ends = 19
+local daylight_ends = 18
 local daylight_starts = 6
 
 local dark = 'dark' -- Override with a dark mode of your choice.
 local light = 'light' -- Override with a light mode of your choice.
 
-local define_colorscheme = function()
+-- Enable modern terminal features before setting the colorscheme.
+-- require 'custom.modern-terminal-support'
+
+local define_colorscheme = function(new_mode)
+  new_mode = new_mode or vim.g.colorscheme_mode
   local ambient_is_dark = hour_now < daylight_starts or hour_now >= daylight_ends
-  if vim.g.colorscheme_mode == dark or ambient_is_dark then
+  if new_mode == dark or ambient_is_dark then
     return vim.g.colorscheme_mode_dark
-  elseif vim.g.colorscheme_mode == light or not ambient_is_dark then
+  elseif new_mode == light or not ambient_is_dark then
     return vim.g.colorscheme_mode_light
   else
     return vim.g.colorscheme_mode_dark
