dc34f0952484297ff58a1473407469f59c473f5e
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-03-24 11:42:28 -0300

    feat(neovim): add night-owl plugin configuration with dynamic path resolution

--- neovim/nvim/lua/custom/plugins/init.lua
+++ neovim/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+local plugin_path = function(plugin_name)
+  return (os.getenv 'MCRA_PROJECTS_FOLDER') .. '/dotfiles/neovim/nvim/lua/custom/my-plugins/' .. plugin_name
+end
+
+return {
+  {
+    dir = plugin_path 'night-owl',
+    name = 'night-owl',
+    config = function()
+      require 'night-owl'
+    end,
+    priority = 1000,
+  },
+}
