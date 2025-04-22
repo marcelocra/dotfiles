f1ccec4d2f6fcc4acb90cc265e4bd4e97e4c7148
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-04-22 02:29:38 -0300

    feat(nvim): add spellchecker

--- neovim/LazyVim/lua/config/options.lua
+++ neovim/LazyVim/lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+local the_leader = " "
+vim.g.mapleader = the_leader
+vim.g.maplocalleader = the_leader
+
+vim.opt.spelllang = "en,pt_br,pt"
