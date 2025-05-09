1cd785520d8c61d809300300ef91f6fba83aeddf
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-05-09 16:50:08 -0300

    fix(nvim): get clojure (conjure) config to work correctly (finally!)

--- neovim/LazyVim/lua/mcra/plugins/clojure-conjure-setup.lua
+++ neovim/LazyVim/lua/mcra/plugins/clojure-conjure-setup.lua
@@ -3,18 +3,11 @@ return {
   {
     "saghen/blink.cmp",
     dependencies = { "saghen/blink.compat", "marcelocra/cmp-conjure" },
-
-    opts = {
-      sources = {
-        -- compat = { "conjure" },
-        compat = { "conjure", "clojure" },
-      },
-    },
+    -- TODO: Try adding "clojure" to `compat` and see if it improves.
+    opts = { sources = { compat = { "conjure" } } },
   },
 
-  -- Add s-exp mappings. Essentially replaces the next two plugins, having the
-  -- same functionality as vim-sexp with the mappings of
-  -- vim-sexp-mappings-for-regular-people.
+  -- Replaces vim-sexp with *-mappings-for-regular-people.
   {
     "marcelocra/nvim-treesitter-sexp",
     dependencies = {
@@ -26,29 +19,66 @@ return {
         opts = { ensure_installed = { "clojure" } },
       },
     },
-    ft = { "clojure", "fennel", "racket" },
+
+    -- INFO: Supported languages. For others, fallback to the ones below.
+    ft = { "clojure", "fennel", "janet", "query" },
     lazy = true,
-    opts = {},
-    -- event = "LazyFile",
+    opts = {
+      keymaps = {
+        motions = {
+          prev_top_level = "[r",
+          next_top_level = "]r",
+        },
+      },
+    },
   },
 
-  -- {
-  --   -- Better Lisp (s-exp) support.
-  --   "guns/vim-sexp",
-  --   ft = { "clojure", "fennel", "racket" },
-  --   lazy = true,
-  -- },
-  --
-  -- {
-  --   -- Better Lisp (s-exp) mappings.
-  --   "tpope/vim-sexp-mappings-for-regular-people",
-  --   ft = { "clojure", "fennel", "racket" },
-  --   lazy = true,
-  -- },
+  -- Better Lisp (s-exp) support.
+  {
+    "guns/vim-sexp",
+    ft = { "racket" },
+    lazy = true,
+    config = function()
+      -- 'vim-sexp' defaults that use Alt (M).
+      -- <M-k>      *<Plug>(sexp_swap_list_backward)*
+      -- <M-j>      *<Plug>(sexp_swap_list_forward)*
+      -- <M-h>      *<Plug>(sexp_swap_element_backward)*
+      -- <M-l>      *<Plug>(sexp_swap_element_forward)*
+      -- <M-S-j>    *<Plug>(sexp_emit_head_element)*
+      -- <M-S-k>    *<Plug>(sexp_emit_tail_element)*
+      -- <M-S-h>    *<Plug>(sexp_capture_prev_element)*
+      -- <M-S-l>    *<Plug>(sexp_capture_next_element)*
+
+      -- Ignore them to avoid conflict with my preferred mappings. Use the
+      -- vim-sexp-mappings-for-regular-people keybindings instead.
+      vim.g["sexp_mappings"] = {
+        ["sexp_swap_list_backward"] = "",
+        ["sexp_swap_list_forward"] = "",
+        ["sexp_swap_element_backward"] = "",
+        ["sexp_swap_element_forward"] = "",
+        ["sexp_emit_head_element"] = "",
+        ["sexp_emit_tail_element"] = "",
+        ["sexp_capture_prev_element"] = "",
+        ["sexp_capture_next_element"] = "",
+
+        -- These doesn't seem to be working.
+        ["sexp_insert_double_quote"] = "",
+        ["sexp_insert_backspace"] = "",
+      }
+    end,
+  },
+
+  -- Better Lisp (s-exp) mappings.
+  {
+    "tpope/vim-sexp-mappings-for-regular-people",
+    ft = { "racket" },
+    lazy = true,
+  },
 
   -- Colorize the output of the log buffer.
   {
     "marcelocra/baleia.nvim",
+    lazy = true,
     opts = {
       line_starts_at = 3,
     },
@@ -66,36 +96,37 @@ return {
   -- Clojure, Lisps, Python, etc, REPL support in Neovim.
   {
     "Olical/conjure",
+    dependencies = {
+      -- "marcelocra/cmp-conjure",
+      -- "marcelocra/baleia.nvim",
+      -- {
+      --   "LazyVim/LazyVim",
+      --
+      --   keys = {
+      --     { "]]", false },
+      --     { "[[", false },
+      --   },
+      --
+      --   init = function()
+      --     --
+      --     -- Remove some LazyVim default mappings before loading Conjure's.
+      --     --
+      --
+      --     vim.keymap.del("n", "]]")
+      --     vim.keymap.del("n", "[[")
+      --   end,
+      -- },
+    },
+
     ft = { "clojure", "edn", "fennel", "python", "racket", "lua" },
     lazy = true,
-    -- event = "LazyFile",
-
-    config = function(_, _)
-      require("conjure.main").main()
-      require("conjure.mapping")["on-filetype"]()
-
-      -- Alternative evaluation mappings for Conjure.
-      vim.keymap.set({ "n" }, "<S-CR>", "<LocalLeader>ew", { silent = true, remap = true })
-      vim.keymap.set({ "n" }, "<M-S-CR>", "<LocalLeader>ecw", { silent = true, remap = true })
-      vim.keymap.set({ "i" }, "<S-CR>", "<Esc><LocalLeader>ewa", { silent = true, remap = true })
-      vim.keymap.set({ "i" }, "<M-S-CR>", "<Esc><LocalLeader>ecwa", { silent = true, remap = true })
-
-      vim.keymap.set({ "n" }, "<C-CR>", "<LocalLeader>ee", { silent = true, remap = true })
-      vim.keymap.set({ "n" }, "<M-C-CR>", "<LocalLeader>ece", { silent = true, remap = true })
-      vim.keymap.set({ "i" }, "<C-CR>", "<Esc><LocalLeader>eea", { silent = true, remap = true })
-      vim.keymap.set({ "i" }, "<M-C-CR>", "<Esc><LocalLeader>ecea", { silent = true, remap = true })
-
-      vim.keymap.set({ "n" }, "<M-CR>", "<LocalLeader>er", { silent = true, remap = true })
-      vim.keymap.set({ "n" }, "<C-S-M-CR>", "<LocalLeader>ecr", { silent = true, remap = true })
-      vim.keymap.set({ "i" }, "<M-CR>", "<Esc><LocalLeader>era", { silent = true, remap = true })
-      vim.keymap.set({ "i" }, "<C-S-M-CR>", "<Esc><LocalLeader>ecra", { silent = true, remap = true })
-
-      vim.keymap.set({ "v" }, "<C-CR>", "<LocalLeader>E", { silent = true, remap = true })
-      vim.keymap.set({ "v" }, "<M-CR>", "<LocalLeader>e!", { silent = true, remap = true })
-    end,
 
     init = function()
-      -- Print color codes if baleia.nvim is available.
+      --
+      -- LazyExtras Clojure config.
+      --
+
+      -- print color codes if baleia.nvim is available
       local colorize = require("lazyvim.util").has("baleia.nvim")
 
       if colorize then
@@ -104,7 +135,7 @@ return {
         vim.g["conjure#log#strip_ansi_escape_sequences_line_limit"] = 1
       end
 
-      -- Disable diagnostics in log buffer and colorize it.
+      -- disable diagnostics in log buffer and colorize it
       vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
         pattern = "conjure-log-*",
         callback = function()
@@ -130,10 +161,14 @@ return {
         end,
       })
 
-      -- Prefer LSP for jump-to-definition and symbol-doc, and use conjure
-      -- alternatives with <LocalLeader>K and <LocalLeader>gd.
+      -- prefer LSP for jump-to-definition and symbol-doc, and use conjure
+      -- alternatives with <localleader>K and <localleader>gd
       vim.g["conjure#mapping#doc_word"] = "K"
       vim.g["conjure#mapping#def_word"] = "gd"
+
+      --
+      -- My previous configs.
+      --
 
       -- Uncomment this to get verbose logging to help diagnose internal Conjure
       -- issues. This is VERY helpful when reporting an issue with the project.
@@ -161,37 +196,42 @@ return {
       -- vim.g["conjure#client#clojure#nrepl#connection#auto_repl#cmd"] = "clj -M:repl/conjure"
     end,
 
-    -- config = function(_, _)
-    --   -- INFO: Using the tree-sitter sexp plugin, these are not necessary.
-    --
-    --   -- 'vim-sexp' defaults that use Alt (M).
-    --   -- <M-k>      *<Plug>(sexp_swap_list_backward)*
-    --   -- <M-j>      *<Plug>(sexp_swap_list_forward)*
-    --   -- <M-h>      *<Plug>(sexp_swap_element_backward)*
-    --   -- <M-l>      *<Plug>(sexp_swap_element_forward)*
-    --   -- <M-S-j>    *<Plug>(sexp_emit_head_element)*
-    --   -- <M-S-k>    *<Plug>(sexp_emit_tail_element)*
-    --   -- <M-S-h>    *<Plug>(sexp_capture_prev_element)*
-    --   -- <M-S-l>    *<Plug>(sexp_capture_next_element)*
-    --
-    --   -- Ignore them to avoid conflict with my preferred mappings. Use the
-    --   -- vim-sexp-mappings-for-regular-people keybindings instead.
-    --   vim.g["sexp_mappings"] = {
-    --     ["sexp_swap_list_backward"] = "",
-    --     ["sexp_swap_list_forward"] = "",
-    --     ["sexp_swap_element_backward"] = "",
-    --     ["sexp_swap_element_forward"] = "",
-    --     ["sexp_emit_head_element"] = "",
-    --     ["sexp_emit_tail_element"] = "",
-    --     ["sexp_capture_prev_element"] = "",
-    --     ["sexp_capture_next_element"] = "",
-    --
-    --     -- These doesn't seem to be working.
-    --     ["sexp_insert_double_quote"] = "",
-    --     ["sexp_insert_backspace"] = "",
-    --   }
-    -- end,
+    config = function()
+      --
+      -- Load Conjure defaults.
+      --
 
+      require("conjure.main").main()
+      require("conjure.mapping")["on-filetype"]()
+
+      --
+      -- Alternative evaluation mappings for Conjure.
+      --
+
+      -- Word mappings (w).
+      vim.keymap.set({ "n" }, "<S-CR>", "<LocalLeader>ew", { silent = true, remap = true })
+      vim.keymap.set({ "n" }, "<M-S-CR>", "<LocalLeader>ecw", { silent = true, remap = true })
+      vim.keymap.set({ "i" }, "<S-CR>", "<Esc><LocalLeader>ewa", { silent = true, remap = true })
+      vim.keymap.set({ "i" }, "<M-S-CR>", "<Esc><LocalLeader>ecwa", { silent = true, remap = true })
+
+      -- Line mappings (e).
+      vim.keymap.set({ "n" }, "<C-CR>", "<LocalLeader>ee", { silent = true, remap = true })
+      vim.keymap.set({ "n" }, "<M-C-CR>", "<LocalLeader>ece", { silent = true, remap = true })
+      vim.keymap.set({ "i" }, "<C-CR>", "<Esc><LocalLeader>eea", { silent = true, remap = true })
+      vim.keymap.set({ "i" }, "<M-C-CR>", "<Esc><LocalLeader>ecea", { silent = true, remap = true })
+
+      -- Root mappings (r).
+      vim.keymap.set({ "n" }, "<M-CR>", "<LocalLeader>er", { silent = true, remap = true })
+      vim.keymap.set({ "n" }, "<C-S-M-CR>", "<LocalLeader>ecr", { silent = true, remap = true })
+      vim.keymap.set({ "i" }, "<M-CR>", "<Esc><LocalLeader>era", { silent = true, remap = true })
+      vim.keymap.set({ "i" }, "<C-S-M-CR>", "<Esc><LocalLeader>ecra", { silent = true, remap = true })
+
+      -- Visual mappings.
+      vim.keymap.set({ "v" }, "<C-CR>", "<LocalLeader>E", { silent = true, remap = true })
+      vim.keymap.set({ "v" }, "<M-CR>", "<LocalLeader>e!", { silent = true, remap = true })
+    end,
+
+    -- -- TODO: Try to make this work.
     -- keys = {
     --   { "<M-j>", false },
     --   { "<M-k>", false },
@@ -218,12 +258,6 @@ return {
     --
     --   -- }}}
     --
-    -- },
-
-    -- -- NOTE: Might not be necessary with the other stuff above.
-    -- dependencies = {
-    --   -- Auto completion integration.
-    --   "marcelocra/cmp-conjure",
     -- },
   },
 }
