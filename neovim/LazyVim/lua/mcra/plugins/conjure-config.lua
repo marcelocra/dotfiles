5686087bf3bc326c474938ace0cb07442773ce92
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-05-09 02:23:35 -0300

    refactor(conjure-config): get it to work properly, with auto complete for conjure

--- neovim/LazyVim/lua/mcra/plugins/conjure-config.lua
+++ neovim/LazyVim/lua/mcra/plugins/conjure-config.lua
@@ -1,27 +1,11 @@
---
--- Defaults merging rules:
---  * cmd: the list of commands will be extended with your custom commands
---  * event: the list of events will be extended with your custom events
---  * ft: the list of filetypes will be extended with your custom filetypes
---  * keys: the list of keymaps will be extended with your custom keymaps
---  * opts: your custom opts will be merged with the default opts
---  * dependencies: the list of dependencies will be extended with your custom
---    dependencies any other property will override the defaults
---
--- For ft, event, keys, cmd and opts you can instead also specify a values
--- function that can make changes to the default values, or return new values to
--- be used instead.
---
--- Docs from:
--- https://www.lazyvim.org/configuration/plugins#%EF%B8%8F-customizing-plugin-specs
---
-
 return {
+
   {
     -- Clojure, Lisps, Python, etc, REPL support in Neovim.
     "Olical/conjure",
-    ft = { "clojure", "fennel", "python", "racket", "lua" }, -- etc
+    ft = { "clojure", "fennel", "python", "racket", "lua" },
     lazy = true,
+
     init = function()
       -- Set configuration options here
       -- Uncomment this to get verbose logging to help diagnose internal Conjure issues
@@ -44,26 +28,47 @@ return {
 
       vim.g["conjure#highlight#enabled"] = true
 
-      -- -- Uses clj with nrepl instead of babashka. Requires an alias named
-      -- -- :repl/conjure in ~/.clojure/deps.edn (mine is in the root of this
-      -- -- repo).
+      -- NOTE: Uses clj with nrepl instead of babashka. Requires an alias named
+      -- :repl/conjure in ~/.clojure/deps.edn (mine is in the root of this
+      -- repo).
       -- vim.g["conjure#client#clojure#nrepl#connection#auto_repl#cmd"] = "clj -M:repl/conjure"
     end,
 
     config = function()
-      -- Ignore <M-j/k> mappings from vim-sexp to avoid conflict with my
-      -- preferred mappings.
+      -- INFO: 'vim-sexp' defaults that use Alt (M).
+
+      -- <M-k>      *<Plug>(sexp_swap_list_backward)*
+      -- <M-j>      *<Plug>(sexp_swap_list_forward)*
+      -- <M-h>      *<Plug>(sexp_swap_element_backward)*
+      -- <M-l>      *<Plug>(sexp_swap_element_forward)*
+      -- <M-S-j>    *<Plug>(sexp_emit_head_element)*
+      -- <M-S-k>    *<Plug>(sexp_emit_tail_element)*
+      -- <M-S-h>    *<Plug>(sexp_capture_prev_element)*
+      -- <M-S-l>    *<Plug>(sexp_capture_next_element)*
+
+      -- Ignore them to avoid conflict with my preferred mappings. Use the
+      -- vim-sexp-mappings-for-regular-people keybindings instead.
       vim.g["sexp_mappings"] = {
         ["sexp_swap_list_backward"] = "",
         ["sexp_swap_list_forward"] = "",
+        ["sexp_swap_element_backward"] = "",
+        ["sexp_swap_element_forward"] = "",
+        ["sexp_emit_head_element"] = "",
+        ["sexp_emit_tail_element"] = "",
+        ["sexp_capture_prev_element"] = "",
+        ["sexp_capture_next_element"] = "",
+
+        -- These doesn't seem to be working.
+        ["sexp_insert_double_quote"] = "",
+        ["sexp_insert_backspace"] = "",
       }
 
       -- Alternative evaluation mappings.
 
       vim.keymap.set({ "n" }, "<S-CR>", "<LocalLeader>ew", { silent = true, remap = true })
-      vim.keymap.set({ "n" }, "<M-S-CR>", "<LocalLeader>ew", { silent = true, remap = true })
+      vim.keymap.set({ "n" }, "<M-S-CR>", "<LocalLeader>ecw", { silent = true, remap = true })
       vim.keymap.set({ "i" }, "<S-CR>", "<Esc><LocalLeader>ewa", { silent = true, remap = true })
-      vim.keymap.set({ "i" }, "<M-S-CR>", "<Esc><LocalLeader>ewa", { silent = true, remap = true })
+      vim.keymap.set({ "i" }, "<M-S-CR>", "<Esc><LocalLeader>ecwa", { silent = true, remap = true })
 
       vim.keymap.set({ "n" }, "<C-CR>", "<LocalLeader>ee", { silent = true, remap = true })
       vim.keymap.set({ "n" }, "<M-C-CR>", "<LocalLeader>ece", { silent = true, remap = true })
@@ -79,35 +84,63 @@ return {
       vim.keymap.set({ "v" }, "<M-CR>", "<LocalLeader>e!", { silent = true, remap = true })
     end,
 
-    -- TODO: Uncomment this once the integration is fixed.
-    -- -- Optional cmp-conjure integration
-    -- dependencies = { 'marcelocra/cmp-conjure' },
+    -- keys = {
+    --   { "<M-j>", false },
+    --   { "<M-k>", false },
+    --
+    --   -- Alternative evaluation mappings. {{{
+    --
+    --   { "<S-CR>", mode = { "n" }, "<LocalLeader>ew" },
+    --   { "<M-S-CR>", mode = { "n" }, "<LocalLeader>ecw" },
+    --   { "<S-CR>", mode = { "i" }, "<Esc><LocalLeader>ewa" },
+    --   { "<M-S-CR>", mode = { "i" }, "<Esc><LocalLeader>ecwa" },
+    --
+    --   { "<C-CR>", mode = { "n" }, "<LocalLeader>ee" },
+    --   { "<M-C-CR>", mode = { "n" }, "<LocalLeader>ece" },
+    --   { "<C-CR>", mode = { "i" }, "<Esc><LocalLeader>eea" },
+    --   { "<M-C-CR>", mode = { "i" }, "<Esc><LocalLeader>ecea" },
+    --
+    --   { "<M-CR>", mode = { "n" }, "<LocalLeader>er" },
+    --   { "<C-S-M-CR>", mode = { "n" }, "<LocalLeader>ecr" },
+    --   { "<M-CR>", mode = { "i" }, "<Esc><LocalLeader>era" },
+    --   { "<C-S-M-CR>", mode = { "i" }, "<Esc><LocalLeader>ecra" },
+    --
+    --   { "<C-CR>", mode = { "v" }, "<LocalLeader>E" },
+    --   { "<M-CR>", mode = { "v" }, "<LocalLeader>e!" },
+    --
+    --   -- }}}
+    --
+    -- },
+
+    dependencies = {
+      -- cmp-conjure integration.
+      "marcelocra/cmp-conjure",
+    },
   },
 
-  -- TODO: Figure out how to use this with blink.
-  -- {
-  --   -- Integrates Conjure with cmp.
-  --   "marcelocra/cmp-conjure",
-  --   lazy = true,
-  --   config = function()
-  --     local cmp = require("cmp")
-  --     local config = cmp.get_config()
-  --     table.insert(config.sources, { name = "conjure" })
-  --     return cmp.setup(config)
-  --   end,
-  -- },
+  {
+    -- cmp-conjure integration.
+    "saghen/blink.cmp",
+    dependencies = { "saghen/blink.compat", "marcelocra/cmp-conjure" },
+
+    opts = {
+      sources = {
+        compat = { "conjure" },
+      },
+    },
+  },
 
   {
     -- Better Lisp (s-exp) support.
     "guns/vim-sexp",
-    ft = { "clojure", "fennel", "racket" }, -- etc
+    ft = { "clojure", "fennel", "racket" },
     lazy = true,
   },
 
   {
     -- Better Lisp (s-exp) mappings.
     "tpope/vim-sexp-mappings-for-regular-people",
-    ft = { "clojure", "fennel", "racket" }, -- etc
+    ft = { "clojure", "fennel", "racket" },
     lazy = true,
   },
 }
