de3a05972f5a6271290beebf10445282fa744311
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-08-20 14:22:15 -0300

    feat: get the pwsh script to mute microphone working

---
+++ pwsh/toggle-mic.bat
@@ -0,0 +1,2 @@
+@echo off
+pwsh.exe -NonInteractive -NoProfile -WindowStyle Hidden -File "C:\projects\dotfiles\pwsh\toggle-mic.ps1"
--- pwsh/toggle-mic.ps1
+++ pwsh/toggle-mic.ps1
@@ -1,4 +1,17 @@
-# Write-Host "Executed!" | Out-File "C:\tmp\debug.txt" -Append
+# "$(Get-Date): Started" | Out-File "C:\tmp\debug.log" -Append
+# "Args: $args" | Out-File "C:\tmp\debug.log" -Append
+
+# Hide the PowerShell window
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+
+$consolePtr = [Console.Window]::GetConsoleWindow()
+[Console.Window]::ShowWindow($consolePtr, 0) # 0 = SW_HIDE
+
 
 Add-Type -TypeDefinition @"
 using System;
@@ -14,3 +27,5 @@ public class AudioControl {
 # Simulate pressing the microphone mute key
 [AudioControl]::keybd_event(0xAD, 0, 0, [UIntPtr]::Zero)
 [AudioControl]::keybd_event(0xAD, 0, 0x0002, [UIntPtr]::Zero)
+
+# "$(Get-Date): Finished" | Out-File "C:\tmp\debug.log" -Append
