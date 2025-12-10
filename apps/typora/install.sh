4b622eb26258169dcf5e006d9ee7fb76abb11bac
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-12-10 11:43:08 -0300

    feat(typora): start adding typora installation

---
+++ apps/typora/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+#
+
+# 1. Download the tar.gz from https://typora.io/#linux
+
+# 2. Verify gpg and sha256sum if possible
+
+# 3. Extract to /opt/typora
+sudo tar xzvf Typora-linux-x64.tar.gz -C /opt/typora
