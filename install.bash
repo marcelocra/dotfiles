e4c1c1440fab2e10b9954d8288d73a1eb62bae2c
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-12-30 19:49:03 +0000

    feat(install.bash): add new skip options for Node.js and npm packages and explicitly set dotfiles skip variables.

--- install.bash
+++ install.bash
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
+# Most common options for machines accessed through SSH.
+# TODO: Update to include options for other common use cases.
 
 set -euo pipefail  # Exit on errors.
 
-export DOTFILES_SKIP_HOMEBREW="${DOTFILES_SKIP_HOMEBREW:-false}"
-export DOTFILES_SKIP_CLI_TOOLS="${DOTFILES_SKIP_CLI_TOOLS:-false}"
-export DOTFILES_SKIP_ZSH_PLUGINS="${DOTFILES_SKIP_ZSH_PLUGINS:-false}"
-export DOTFILES_SKIP_VSCODE="${DOTFILES_SKIP_VSCODE:-true}"
-export DOTFILES_SKIP_EDITOR_LAUNCHER="${DOTFILES_SKIP_EDITOR_LAUNCHER:-true}"
-export DOTFILES_SKIP_GIT_SHIMS="${DOTFILES_SKIP_GIT_SHIMS:-false}"
-export DOTFILES_SKIP_SSH_CONFIG="${DOTFILES_SKIP_SSH_CONFIG:-false}"
+export DOTFILES_SKIP_HOMEBREW="false"
+export DOTFILES_SKIP_CLI_TOOLS="false"
+export DOTFILES_SKIP_ZSH_PLUGINS="false"
+export DOTFILES_SKIP_VSCODE="true"
+export DOTFILES_SKIP_EDITOR_LAUNCHER="true"
+export DOTFILES_SKIP_GIT_SHIMS="false"
+export DOTFILES_SKIP_SSH_CONFIG="false"
+export DOTFILES_SKIP_NODE_LTS="false"
+export DOTFILES_SKIP_NPM_PACKAGES="false"
+
 export DOTFILES_DIR="$HOME/x/dotfiles"
 export DOTFILES_DEBUG="1"
 
